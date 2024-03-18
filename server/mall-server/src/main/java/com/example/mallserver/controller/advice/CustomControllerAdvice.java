73eba4e0b5f43ba7111d6a2db6baed32ea0bc238
jhyun <jkwhd12@gmail.com>
2024-03-18 17:14:37 +0900

    @RestControllerAdvice

---
+++ server/mall-server/src/main/java/com/example/mallserver/controller/advice/CustomControllerAdvice.java
@@ -0,0 +1,27 @@
+package com.example.mallserver.controller.advice;
+
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class CustomControllerAdvice {
+	// 공통으로 쓸려고 그리고 exception handler 만들어 줄려고 씁니다.
+
+	@ExceptionHandler(NoSuchElementException.class)
+	// 리턴 타입은 사실 상태코드를 만들어줘야하는데
+	// 이를 세밀하게 하고싶으면 사실 ResponseEntity좋아요
+	public ResponseEntity<?> notExist(NoSuchElementException e) {
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("msg", e.getMessage()));
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<?> notExist(MethodArgumentNotValidException e) {
+		return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).body(Map.of("msg", e.getMessage()));
+	}
+}
