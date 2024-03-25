a4545fe775039244816c785e5706254ae327c7b3
jhyun <jkwhd12@gmail.com>
2024-03-25 15:56:52 +0900

    로그인 성공과 실패 처리 successHandler failureHandler

--- server/mall-server/src/main/java/com/example/mallserver/config/CustomSecurityConfig.java
+++ server/mall-server/src/main/java/com/example/mallserver/config/CustomSecurityConfig.java
@@ -6,12 +6,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.example.mallserver.security.handler.APILoginFailHandler;
+import com.example.mallserver.security.handler.APILoginSuccessHandler;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -39,6 +43,13 @@ public class CustomSecurityConfig {
 		// 잠깐 쓰는거임
 		http.formLogin(httpSecurityFormLoginConfigurer -> {
 			httpSecurityFormLoginConfigurer.loginPage("/api/member/login");
+			httpSecurityFormLoginConfigurer.successHandler(new APILoginSuccessHandler());
+			httpSecurityFormLoginConfigurer.failureHandler(new APILoginFailHandler());
+		});
+
+		// 세션 사용안하겠다는 설정
+		http.sessionManagement(httpSecuritySessionManagementConfigurer -> {
+			httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.NEVER);
 		});
 
 		return http.build();
